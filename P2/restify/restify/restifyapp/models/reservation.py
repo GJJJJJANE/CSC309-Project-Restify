f09427f2f41bc2953b735e9e54a48e94c09a16dd
GJJJJJANE <jinggao1023@gmail.com>
2023-03-10 14:09:30 -0500

    add models

---
+++ P2/restify/restify/restifyapp/models/reservation.py
@@ -0,0 +1,41 @@
+# import djangos
+from django.db import models
+
+# import other models
+from user import User
+from property import Property
+
+class Reservation(models.Model):
+    # STATUS
+    PENDING = 'pe'
+    DENIED = 'de'
+    EXPIRED = 'ex'
+    APPROVED = 'ap'
+    CANCELED = 'ca'
+    TERMINATED = 'te'
+    COMPLETED = 'co'
+
+    STATUS_CHOICES = [
+        ('pe', 'Pending'),
+        ('de', 'Denied'),
+        ('ex', 'Expired'),
+        ('ap', 'Approved'),
+        ('ca', 'Canceled'),
+        ('te', 'Terminated'),
+        ('co', 'Completed'),
+        (None, 'Reservation status')
+    ]
+
+    # FIELDS
+    # id is set to default int pkey
+    # Foreign keys
+    guest = models.ForeignKey('User', on_delete=models.CASCADE)
+    property = models.ForeignKey('Property',on_delete=models.CASCADE) # OR use number id?
+    # reservation state
+    state = models.CharField(max_length=2, choices=STATUS_CHOICES)
+    # reservation duration
+    start = models.DateField()
+    end = models.DateField()
+
+    class Meta:
+        ordering = ['start']
