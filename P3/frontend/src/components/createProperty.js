651a0e3023bc2b9ca352d5ee043af88d9da88ae8
Waannng <queenie.wang@mail.utoronto.ca>
2023-04-10 17:50:49 -0400

    add some const

--- P3/frontend/src/components/createProperty.js
+++ P3/frontend/src/components/createProperty.js
@@ -6,10 +6,45 @@ import { Form, Button, Container, Col, Row, ToggleButtonGroup, ToggleButton } fr
 
 
 const CreatePropertyForm = () => {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [roomdes, setRoomdes] = useState("");
+    const [location, setLocation] = useState("");
+    const [numGuests, setNumGuests] = useState(0);
+    const [numBeds, setNumBeds] = useState(0);
+    const [numBaths, setNumBaths] = useState(0);
+    const [amenities, setAmenities] = useState("");
+    const [price, setPrice] = useState(0);
+    const [start, setStart] = useState("");
+    const [end, setEnd] = useState("");
+    
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        if (!title || !numGuests) {
+          alert("Please fill in all fields.");
+          return;
+        }
+        const propertyData = new FormData();
+        propertyData.append("title", title);
+        propertyData.append("num_guests", numGuests);
+        //formData.append("amenities", amenities);
+        //images.forEach((image) => formData.append("images", image));
+        try {
+          const response = await axios.post("", propertyData, {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          });
+          console.log(response.data);
+        } catch (error) {
+          console.log(error);
+        }
+    };
   
-  return (
+    return (
     <div class="container px-5">
-    <form>
+    <form onSubmit={handleSubmit}>
         <div class ="row">
             <div class="col-md-5 ms-5 mt-5">
                 <h1>   </h1>
@@ -31,7 +66,7 @@ const CreatePropertyForm = () => {
             </div>
             
             <div class="input-group-lg col-md-10 my-5">
-                <input type="text" class="form-control" id="title" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" placeholder="Enter your listing title..." />
+                <input type="text" class="form-control" id="title" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" placeholder="Enter your listing title..." value={title} onChange={(event) => setTitle(event.target.value)} />
             </div>
         </div>
 
@@ -39,7 +74,7 @@ const CreatePropertyForm = () => {
             <div class="justify-content-md-center">
                 <br/>
                 <br/>
-                  <h3>Listing Details</h3>
+                  <h4>Listing Details</h4>
                 <hr class="mt-2"/>
                   <h4>Photos</h4>
                   <br/>
@@ -78,7 +113,7 @@ const CreatePropertyForm = () => {
                   <h4>Description</h4>
                   <br/>
                   <div class="mb-3">
-                    <textarea class="form-control" id="description" rows="8" placeholder="Describe your property..."></textarea>
+                    <textarea class="form-control" id="description" rows="6" placeholder="Describe your property..." value={description} onChange={(event) => setDescription(event.target.value)} />
                   </div>
 
                   <h4>Basics</h4>
@@ -87,44 +122,32 @@ const CreatePropertyForm = () => {
                     <h5>Location</h5>
                     <br/>
                     <div class="input-group mb-3">
-                        <span class="input-group-text" id="country">Country / Region</span>
-                        <input type="text" class="form-control"/>
-                    </div>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text" id="city">City</span>
-                        <input type="text" class="form-control"/>
-                    </div>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text" id="street">Street</span>
-                        <input type="text" class="form-control"/>
-                    </div>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text" id="apt">Apt, Suite (optional)</span>
-                        <input type="text" class="form-control"/>
+                        <span class="input-group-text" id="location">Enter your location</span>
+                        <input type="text" class="form-control" value={location} onChange={(event) => setLocation(event.target.value)} />
                     </div>
 
                     <h5>Number of Guests</h5>
                     <br/>
                     <div class="input-group mb-3">
-                        <input type="text" class="form-control"/>
+                        <input type="number" class="form-control" value={numGuests} onChange={(event) => setNumGuests(event.target.value)} />
                         <span class="input-group-text" id="people">people</span>
                     </div>
                     <br/>
                     <h5>Rooms</h5>
                     <div class="row">
                         <div class="input-group mb-3">
-                            <input type="text" class="form-control"/>
+                            <input type="text" class="form-control" value={numBeds} onChange={(event) => setNumBeds(event.target.value)} />
                             <span class="input-group-text" id="bed">bedrooms</span>
                         </div>
                         <div class="input-group mb-3">
-                            <input type="text" class="form-control"/>
+                            <input type="text" class="form-control" value={numBaths} onChange={(event) => setNumBaths(event.target.value)} />
                             <span class="input-group-text" id="bath">bathrooms</span>
                         </div>
                     </div>
                     <br/>
                     <div class="mb-3">
                         <label for="roomdescription" class="form-label">More description for rooms</label>
-                        <textarea class="form-control" id="rromdescription" rows="6" placeholder="Describe your rooms..."></textarea>
+                        <textarea class="form-control" id="rromdescription" rows="4" placeholder="Describe your rooms..." value={roomdes} onChange={(event) => setRoomdes(event.target.value)} />
                     </div>
 
                     <h4>Amenities</h4>
@@ -160,16 +183,16 @@ const CreatePropertyForm = () => {
                             <label for="price" class="form-label">Price   </label>
                             <div class="input-group">
                                 <span class="input-group-text">$</span>
-                                <input type="text" class="form-control" id="price"/>
+                                <input type="text" class="form-control" id="price" value={price} onChange={(event) => setPrice(event.target.value)} />
                             </div>
                         </div>
                         <div class="col-md-4">
-                            <label for="from" class="form-label">From</label>
-                            <input type="date" class="form-control" id="from"/>
+                            <label for="from" class="form-label">Available start from</label>
+                            <input type="date" class="form-control" id="from" value={start} onChange={(event) => setStart(event.target.value)} />
                         </div>
                         <div class="col-md-4">
                             <label for="to" class="form-label">To</label>
-                            <input type="date" class="form-control" id="to"/>
+                            <input type="date" class="form-control" id="to" value={end} onChange={(event) => setEnd(event.target.value)} />
                         </div>
                     </div>
                     <br/>
@@ -180,15 +203,11 @@ const CreatePropertyForm = () => {
                     <br/>
                     <h5>House rules</h5>
                     <div class="mb-3">
-                        <textarea class="form-control" id="description" rows="5"></textarea>
-                    </div>
-                    <h5>Safety & Property</h5>
-                    <div class="mb-3">
-                        <textarea class="form-control" id="description" rows="5"></textarea>
+                        <textarea class="form-control" id="description" rows="4"></textarea>
                     </div>
                     <h5>Cancellation policy</h5>
                     <div class="mb-3">
-                        <textarea class="form-control" id="description" rows="5"></textarea>
+                        <textarea class="form-control" id="description" rows="4"></textarea>
                     </div>
             </div>
         </div>
