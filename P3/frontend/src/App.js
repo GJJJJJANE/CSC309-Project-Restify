edbcb67b88b4a23ed7c52e9b38b2b5e54488cc57
Waannng <queenie.wang@mail.utoronto.ca>
2023-04-11 20:42:10 -0400

    resolve conflicts

--- P3/frontend/src/App.js
+++ P3/frontend/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import Create from './pages/createPage'
 import Edit from './pages/editPage'
+import Reserve_Host from './pages/Reserve_Host';
+import Reserve_Guest from './pages/Reserve_Guest';
+import PropertyList from './pages/listing';
 
 
 function App() {
@@ -11,6 +14,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Create />} />
         <Route path="/edit" element={<Edit />} />
+        <Route path="/reservations/hostview/" element={<Reserve_Host />} />
+        <Route path="/reservations/guestview/" element={<Reserve_Guest />} />
+        <Route path="/list" element={<PropertyList />} />
       </Routes>
     </BrowserRouter>
   );
