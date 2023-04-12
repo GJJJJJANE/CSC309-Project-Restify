5913de3189d0c0d8944344d86e6b43111095ecc4
Waannng <queenie.wang@mail.utoronto.ca>
2023-04-11 20:34:37 -0400

    property listing + pagination finished

--- P3/frontend/src/App.js
+++ P3/frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import Create from './pages/createPage'
 import Edit from './pages/editPage'
+import PropertyList from './pages/listing'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Create />} />
         <Route path="/edit" element={<Edit />} />
+        <Route path="/list" element={<PropertyList />} />
       </Routes>
     </BrowserRouter>
   );
