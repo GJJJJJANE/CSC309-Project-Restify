f450b4ac4022444792e453fb20dc27b565868eab
GJJJJJANE <jinggao1023@gmail.com>
2023-04-16 00:38:24 -0400

    handle pagination

--- P3/frontend/src/pages/GuestCommentList/index.jsx
+++ P3/frontend/src/pages/GuestCommentList/index.jsx
@@ -1,17 +1,20 @@
 import React, {  useState, useEffect, Component } from 'react';
 import GuestComment from '../../components/GuestComment';
 import { useParams } from "react-router-dom";
+import Pagination from 'react-bootstrap/Pagination';
 import axios from "axios";
 
 const GuestCommentPage = () => {
     const id = useParams();
     const [context, setContext] = useState([])
     const [search, setSearch] = useState("") // query param
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(0);
 
     useEffect (() => {
         const guest_id = id["guestid"]
         try {
-            const response = axios.get(`http://127.0.0.1:8000/comments/${guest_id}/Guestview/?score=${search}`, {
+            const response = axios.get(`http://127.0.0.1:8000/comments/${guest_id}/Guestview/?score=${search}&page=${currentPage}`, {
               headers: {
                   "Access-Control-Allow-Origin": 'http://localhost:3000',
                   "Access-Control-Allow-Credentials": 'true',
@@ -19,16 +22,29 @@ const GuestCommentPage = () => {
             })
             .then(response =>{
                 setContext(response.data.results)
-                // note next and previous for pagination support
+                setTotalPages(Math.ceil(response.data.count / 6))
                 console.log(response.data);
             });
           } catch (error) {
             console.log(error);
           }
-    }, [search]);
+    }, [search, currentPage]);
+
+function handlePageChange(pageNumber) {
+      setCurrentPage(pageNumber);
+  }
+
+  let items = [];
+  for (let number = 1; number <= totalPages; number++) {
+      items.push(
+      <Pagination.Item key={number} onClick={() => handlePageChange(number)} className={number === currentPage ? "active" : ""}>
+          {number}
+      </Pagination.Item>,);
+  }
 
     return <main>
     <GuestComment context={context} search={search} setSearch={setSearch}/>
+    <Pagination className="d-flex justify-content-center">{items}</Pagination>
     </main>;
 }
 
